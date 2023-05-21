feef681880926c361a0831be7a85b8312bde845c
gustavo-exe <gustavoexe136@gmail.com>
2023-05-21 10:43:44 -0600

    pagina de contador

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_test/src/pages/home.dart';
+import 'package:flutter_application_test/src/pages/contador.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -7,8 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-        home: Center(
-      child: Home(),
-    ));
+      home: Center(child: Contador()),
+    );
   }
 }
