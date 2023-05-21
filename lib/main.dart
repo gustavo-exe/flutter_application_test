9f67e144c524bf3a8c44dd2a531de8f1877779d5
gustavo-exe <gustavoexe136@gmail.com>
2023-05-21 16:32:36 -0600

    Imagen local y network

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,40 @@
 import 'package:flutter/material.dart';
-import 'src/app.dart';
 
 void main() {
   runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: const Text("Images"),
+            backgroundColor: Colors.red,
+          ),
+          body: Container(
+              color: Colors.black87,
+              child: Center(
+                child: SizedBox(
+                  width: 380,
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        FittedBox(
+                          fit: BoxFit.fill,
+                          child: Image.asset("assets/ZENITSU.jpg"),
+                        ),
+                        FittedBox(
+                          fit: BoxFit.fill,
+                          child: Image.network(
+                              "https://i.ibb.co/bRfjn23/TANJIRO.jpg"),
+                        )
+                      ]),
+                ),
+              ))),
+    );
+  }
 }
